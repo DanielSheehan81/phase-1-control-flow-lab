9c98aed5dd9061c10611cab244e6c0b066ce17f5
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-12 17:42:07 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) {
+    return 'This one is on me!';
+  } else if (2000 <= depth && depth < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (depth >= 2500) 
+    return 'No can do.';
+  }
+  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  return (city === 'NYC'? 'Ok, sounds good.' : 'No go.');
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default: return 'Bye.';
+  }
   // Write your code here!
 }
